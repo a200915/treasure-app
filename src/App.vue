acca738481dbfd183c993b9cf9f140673da8ccec
dingpeiwen <1529429155@qq.com>
2023-12-18 21:03:31 +0800

    feat:引入element-plus组件内国际化

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <router-view></router-view>
+  <el-config-provider :locale="elLang">
+    <router-view></router-view>
+  </el-config-provider>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import { useLocaleStore } from "@/store/modules/locales";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import en from "element-plus/dist/locale/en.mjs";
+const elLang = computed(() => {
+  return useLocaleStore().locale === "zh" ? zhCn : en;
+});
 </script>
 
 <style scoped></style>
